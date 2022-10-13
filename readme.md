e2245e04119933bac7c03da65198495809bd7706
cgxxv <cgsixv@gmail.com>
2022-10-13 16:49:23 +0800

    Init create react manually

---
+++ readme.md
@@ -0,0 +1,26 @@
+# todo-java-react
+
+## About
+
+I learned java a couple of years ago, but in my job java is not the main program language.
+
+So I decide to write a little project for some evidence by my java skill and for a new job.
+
+React is my favorite frontend framework, I use it for my sparetime project very often.
+
+This is a todo graffiti project target at practice for java, springmvc and react-18.
+
+BTW, I have written another todo in pure javascript several years ago.
+[cgxxv/todomvc](https://github.com/cgxxv/todomvc)
+
+
+## Tips
+
+A tool for intellij idea.
+[ja-netfilter-all](https://github.com/libin9iOak/ja-netfilter-all)
+
+
+## For a new job
+
+I am a full-stack developer, skilled and experenced at multiple languages and areas.
+So anyone interested at me, please contact me by my gmail.
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {SourceMapDevToolPlugin} = require("webpack");
+
+const config = {
+    entry: {
+        index: "./web_src/index.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "js/[name].js",
+    },
+    devServer: {
+        compress: true,
+        port: 9000,
+        open: true,
+        proxy: {
+            "/api": "http://localhost:8088",
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [["@babel/preset-react", {runtime: "automatic"}]]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asset',
+                resourceQuery: /url/, // *.svg?url
+            },
+            {
+                test: /\.svg$/,
+                issuer: /\.[jt]sx?$/,
+                resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+                use: [
+                    {
+                        loader: "@svgr/webpack",
+                        options: {icon: true},
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+        new SourceMapDevToolPlugin({
+            filename: "[file].[contenthash:8].map",
+            include: ["js/index.js", "css/index.css"],
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+        }),
+    ],
+    performance: {
+        hints: false,
+        maxEntrypointSize: Infinity,
+        maxAssetSize: Infinity,
+    }
+}
+
+module.exports = (env, argv) => {
+    if (argv.mode === "development") {
+        config.mode = "development";
+        config.devtool = "eval-source-map";
+    }
+
+    if (argv.mode === "production") {
+        config.mode = "production";
+        config.devtool = "source-map";
+    }
+
+    return config;
+}
